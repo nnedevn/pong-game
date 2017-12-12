abad3e9ff646a1684bd95ea98dfe9d64eb4bc45d
Nick Nedev <nedevn@gmail.com>
2017-12-12 09:32:09 -0800

    Adds AI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let PADDLE_HEIGHT = 100;
+let paddleWidth = 10;
 
 let canvas;
 let context;
@@ -9,9 +10,13 @@ let ballY = 50;
 let ballSpeedY = 5;
 
 let paddle1Y = 250;
+let paddle2Y = 250;
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
 window.onload = function() {
-  var framesPerSecond = 30;
+  var framesPerSecond = 50;
   canvas = document.getElementById('canvas');
   context = canvas.getContext('2d');
 
@@ -23,19 +28,25 @@ window.onload = function() {
   canvas.addEventListener('mousemove', (evt) => {
     var mousePos = calculateMousePosition(evt);
     paddle1Y = mousePos.y;
-  });
 
+  });
 
   function drawEverything() {
     drawColorRect(0, 0, canvas.width, canvas.height, 'black');
     // left player paddle
-    drawColorRect(0, paddle1Y - PADDLE_HEIGHT / 2, 10, 100, 'white');
-    //drawColorRect(ballX, 100, 10, 10, 'red');
+    drawColorRect(0, paddle1Y - PADDLE_HEIGHT, paddleWidth, PADDLE_HEIGHT, 'white');
+    // right player paddle
+    drawColorRect(canvas.width - paddleWidth, paddle2Y, paddleWidth, PADDLE_HEIGHT, 'white');
     // ball
-    drawColorCircle(ballX, ballY, 10, 'white');
+    drawColorCircle(ballX, ballY, 10, 'red');
+    context.fillText(playerOneScore, 100, 100);
+    context.fillText(playerOneScore, canvas.width - 100, 100);
   }
-
+  /*
+  
+   */
   function ballReset() {
+    ballSpeedX = -ballSpeedX;
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
   }
@@ -55,44 +66,63 @@ window.onload = function() {
     }
   }
 
+  function computerMovement() {
+    var paddle2YCenter = paddle2Y + PADDLE_HEIGHT / 2;
+    if (paddle2YCenter < ballY - 35) {
+      paddle2Y += 6;
+    } else {
+      paddle2Y -= 6;
+    }
+  }
+
   function moveEverything() {
+    computerMovement();
     ballX += ballSpeedX;
     ballY += ballSpeedY;
+    // console.log('position y:', ballY);
 
+    // ball passes the right side. 
     if (ballX > canvas.width) {
+
+      if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
+        ballSpeedX = -ballSpeedX;
+      } else {
+        ballReset();
+        playerOneScore += 1;
+      }
+    }
+
+  }
+  // ball gets past the left side 
+  if (ballX < 0) {
+    if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
       ballSpeedX = -ballSpeedX;
+    } else {
+     
+      ballReset();
+       playerTwoScore += 1;
     }
-
-    if (ballX < 0) {
-      ballSpeedX = -ballSpeedX;
-      ballreset();
-    }
-
-    if (ballY > canvas.height) {
-      ballSpeedY = -ballSpeedY;
-    }
-
-    if (ballX < 0) {
-      ballSpeedY = -ballSpeedY;
-    }
-
   }
 
-
-  function drawColorCircle(centerX, centerY, radius, drawColor) {
-    context.fillStyle = drawColor;
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-    context.fill();
-
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
   }
 
-  function drawColorRect(leftX, topY, width, height, drawColor) {
-    context.fillStyle = drawColor;
-    context.fillRect(leftX, topY, width, height);
-
+  if (ballY < 0) {
+    ballSpeedY = -ballSpeedY;
   }
 
+}
 
 
+function drawColorCircle(centerX, centerY, radius, drawColor) {
+  context.fillStyle = drawColor;
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+  context.fill();
+}
+
+function drawColorRect(leftX, topY, width, height, drawColor) {
+  context.fillStyle = drawColor;
+  context.fillRect(leftX, topY, width, height);
 }
